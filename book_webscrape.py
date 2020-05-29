ba36ad0cd54171f6e0329ec79ae7e4b6daa2f075
Matt Holmes <holmesm8@gmail.com>
2020-05-29 10:44:51 -0600

    Update to change book webscrape into json object

--- book_webscrape.py
+++ book_webscrape.py
@@ -3,21 +3,17 @@ import requests
 import json
 
 def getBooks(url):
-  res = requests.get(url)
-  res.raise_for_status
+    res = requests.get(url)
+    res.raise_for_status
 
-  soup = bs4.BeautifulSoup(res.text, 'html.parser')
-  elems = soup.find_all("a", class_="bookTitle")[0:10]
-  elems = [i.text for i in elems]
-  return elems
-
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    elems = soup.find_all("a", class_="bookTitle")[0:10]
+    elems = [i.text for i in elems]
+    return elems
 
 books = getBooks('https://www.goodreads.com/shelf/show/trending')
 
-print(len(books))
-print(books)
-
-hashy = {'book1': f'{books[0]}',
+trending_books = {'book1': f'{books[0]}',
          'book2': f'{books[1]}',
          'book3': f'{books[2]}',
          'book4': f'{books[3]}',
@@ -28,3 +24,6 @@ hashy = {'book1': f'{books[0]}',
          'book9': f'{books[8]}',
          'book10': f'{books[9]}',
          }
+
+json_object_trending_books = json.dumps(trending_books)
+print(json_object_trending_books)

88b3de13696b48da35aef35c686f60f0b7f33165
William Meighan <williamdmeighan@gmail.com>
2020-05-30 18:58:45 -0600

    Update json response for FE manipulation

--- wsgi.py
+++ wsgi.py
@@ -1,46 +1,103 @@
 from flask import Flask, render_template, request
+from datetime import date
 import requests
 import json
+
 
 app = Flask(__name__)
 
 @app.route('/weather', methods=['POST'])
-def weather():
-    zipcode = request.form['zip']
 
-    r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+'&appid=c5ec3e05ed22c969db668578d373540a')
+def weather():
+    today = date.today()
+    city = request.form['city']
+
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=c5ec3e05ed22c969db668578d373540a')
+    r2 = requests.get('http://api.eventful.com/json/events/search?...&location='+city+'&date=Today&within=10&app_key=pz8fmcjnBKqnM8rw')
 
     json_object = r.json()
+    json_object2 = r2.json()
 
-    name = json_object['name']
+    try:
+        temp_unrounded = (float(json_object['main']['temp']) * (9/5) - 459.67)
+        temp = round(temp_unrounded, 2)
 
-    description = json_object['weather'][0]['description']
+        temp_high_unrounded = (float(json_object['main']['temp_max']) * (9/5) - 459.67)
+        temp_high = round(temp_high_unrounded, 2)
 
-    temp_unrounded = (float(json_object['main']['temp']) * (9/5) - 459.67)
-    temp = round(temp_unrounded, 2)
+        temp_low_unrounded = (float(json_object['main']['temp_min']) * (9/5) - 459.67)
+        temp_low = round(temp_low_unrounded, 2)
 
-    temp_high_unrounded = (float(json_object['main']['temp_max']) * (9/5) - 459.67)
-    temp_high = round(temp_high_unrounded, 2)
+        final = { 'forcast':
+                    {
+                    'city': json_object['name'],
+                    'description': json_object['weather'][0]['description'],
+                    'current_temp': temp,
+                    'high': temp_high,
+                    'low': temp_low,
+                    'sunrise': json_object['sys']['sunrise'],
+                    'sunset': json_object['sys']['sunset']
+                    },
+                  'events':
+                    { 'event_one' :
+                        {
+                        'name': json_object2['events']['event'][0]['title'],
+                        'url': json_object2['events']['event'][0]['url']
+                        },
+                     'event_two' :
+                        {
+                        'name': json_object2['events']['event'][1]['title'],
+                        'url': json_object2['events']['event'][1]['url']
+                        },
+                     'event_three' :
+                        {
+                        'name': json_object2['events']['event'][2]['title'],
+                        'url': json_object2['events']['event'][2]['url']
+                        },
+                     'event_four' :
+                        {
+                        'name': json_object2['events']['event'][3]['title'],
+                        'url': json_object2['events']['event'][3]['url']
+                        },
+                     'event_five' :
+                        {
+                        'name': json_object2['events']['event'][4]['title'],
+                        'url': json_object2['events']['event'][4]['url']
+                        },
+                     'event_six' :
+                        {
+                        'name': json_object2['events']['event'][5]['title'],
+                        'url': json_object2['events']['event'][5]['url']
+                        },
+                     'event_seven' :
+                        {
+                        'name': json_object2['events']['event'][6]['title'],
+                        'url': json_object2['events']['event'][6]['url']
+                        },
+                     'event_eight' :
+                        {
+                        'name': json_object2['events']['event'][7]['title'],
+                        'url': json_object2['events']['event'][7]['url']
+                        },
+                     'event_nine' :
+                        {
+                        'name': json_object2['events']['event'][8]['title'],
+                        'url': json_object2['events']['event'][8]['url']
+                        },
+                     'event_ten' :
+                        {
+                        'name': json_object2['events']['event'][9]['title'],
+                        'url': json_object2['events']['event'][9]['url']
+                        }
+                    }
+                }
 
-    temp_low_unrounded = (float(json_object['main']['temp_min']) * (9/5) - 459.67)
-    temp_low = round(temp_low_unrounded, 2)
+        final_json = json.dumps(final)
 
-    sunrise = json_object['sys']['sunrise']
+        return final_json
 
-    sunset = json_object['sys']['sunset']
-
-    final = { 'city': name,
-            'description': description,
-            'current_temp': temp,
-            'high': temp_high,
-            'low': temp_low,
-            'sunrise': sunrise,
-            'sunset': sunset
-            }
-
-    final_json = json.dumps(final)
-
-    return render_template('weather.html', final_json=final_json)
+    except:
+        return "We could not locate a city by that name."
 
 @app.route('/')
 def index():

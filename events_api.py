d40f0139f21584669bfdfc54d1f521e8f6b32251
Matt Holmes <holmesm8@gmail.com>
2020-06-01 21:42:20 -0600

    Removed the variables leftover from testing

--- events_api.py
+++ events_api.py
@@ -1,8 +1,5 @@
 import requests
 
-lat = "34.4208"
-long = "-119.6982"
-
 def getEvents(lat, long):
   events_request = requests.get('http://api.eventful.com/json/events/search?...&where='+lat+','+long+'&within=20&app_key=pz8fmcjnBKqnM8rw')
   events_json = events_request.json()

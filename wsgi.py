add3e68feeb27be383f9cbc2f79be910278d1acd
Matt Holmes <holmesm8@gmail.com>
2020-06-01 21:31:47 -0600

    Refactor wsgi so that it is calling the helper api file and the two webscraping files

--- wsgi.py
+++ wsgi.py
@@ -6,6 +6,9 @@ import requests
 import json
 import bs4
 import random
+import events_api
+import book_webscrape
+import movie_webscraper
 
 app = Flask(__name__)
 
@@ -15,12 +18,9 @@ def weather(lat, long):
     weather_request = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+long+'&appid=c5ec3e05ed22c969db668578d373540a&units=imperial')
     weather_json = weather_request.json()
 
-    if weather_json['daily'][0]['temp']['day'] > 60 and (weather_json['current']['weather'][0]['main'] == "Clear" or weather_json['current']['weather'][0]['main'] == "Clouds"):
+    if weather_json['daily'][0]['temp']['day'] > 0 and (weather_json['current']['weather'][0]['main'] == "Clear" or weather_json['current']['weather'][0]['main'] == "Clouds"):
 
-        events_request = requests.get('http://api.eventful.com/json/events/search?...&where='+lat+','+long+'&within=20&app_key=pz8fmcjnBKqnM8rw')
-        events_json = events_request.json()
-
-
+        five_events = events_api.getEvents(lat, long)
         final = {
                 'current': str(weather_json['current']['temp']) + '°F',
                 'sunrise': datetime.datetime.fromtimestamp(weather_json['current']['sunrise']).strftime('%I:%M %p').lstrip('0'),
@@ -150,69 +150,15 @@ def weather(lat, long):
                         }],
                     }],
                 'notifications':
-                    {
-                    'event_one':
-                        {
-                        'name': events_json['events']['event'][0]['title'],
-                        'url': events_json['events']['event'][0]['url']
-                        },
-                    'event_two':
-                        {
-                        'name': events_json['events']['event'][1]['title'],
-                        'url': events_json['events']['event'][1]['url']
-                        },
-                    'event_three':
-                        {
-                        'name': events_json['events']['event'][2]['title'],
-                        'url': events_json['events']['event'][2]['url']
-                        },
-                    'event_four':
-                        {
-                        'name': events_json['events']['event'][3]['title'],
-                        'url': events_json['events']['event'][3]['url']
-                        },
-                    'event_five':
-                        {
-                        'name': events_json['events']['event'][4]['title'],
-                        'url': events_json['events']['event'][4]['url']
-                        }
-                    }
+                    five_events
                 }
-
+                
         final_json = json.dumps(final)
         return final_json
 
     else:
-
-        # def getBooks(url):
-        res = requests.get('https://www.goodreads.com/shelf/show/trending')
-        res.raise_for_status
-
-        soup = bs4.BeautifulSoup(res.text, 'html.parser')
-        elems = soup.find_all("a", class_="bookTitle")[0:10]
-        books = [i.text for i in elems]
-
-        # def getMovies()
-        url = 'http://www.imdb.com/chart/top'
-        response = requests.get(url)
-        soup = bs4.BeautifulSoup(response.text, 'html.parser')
-        movies = soup.select('td.titleColumn')
-        links = [a.attrs.get('href') for a in soup.select('td.titleColumn a')]
-        crew = [a.attrs.get('title') for a in soup.select('td.titleColumn a')]
-        ratings = [b.attrs.get('data-value')
-              for b in soup.select('td.posterColumn span[name=ir]')]
-        imdb = []
-
-        # Movie List Creation
-        for index in range(0, len(movies)):
-            movie_string = movies[index].get_text()
-            movie = (' '.join(movie_string.split()).replace('.', ''))
-            movie_title = movie[len(str(index))+1:-7]
-            place = movie[:len(str(index))-(len(movie))]
-            imdb.append(movie_title)
-
-        random_movies = random.sample(imdb, 10)
-
+        books = book_webscrape.getBooks()
+        random_movies = movie_webscraper.getMovies()
         final = {
                 'current': str(weather_json['current']['temp']) + '°F',
                 'sunrise': datetime.datetime.fromtimestamp(weather_json['current']['sunrise']).strftime('%I:%M %p').lstrip('0'),
@@ -344,40 +290,13 @@ def weather(lat, long):
                 'notifications':
                     {
                     'books':
-                        {
-                        'book1': f'{books[0]}',
-                        'book2': f'{books[1]}',
-                        'book3': f'{books[2]}',
-                        'book4': f'{books[3]}',
-                        'book5': f'{books[4]}',
-                        'book6': f'{books[5]}',
-                        'book7': f'{books[6]}',
-                        'book8': f'{books[7]}',
-                        'book9': f'{books[8]}',
-                        'book10': f'{books[9]}',
-                        },
+                        books,
                     'movies':
-                        {
-                        'movie1': f'{random_movies[0]}',
-                        'movie2': f'{random_movies[1]}',
-                        'movie3': f'{random_movies[2]}',
-                        'movie4': f'{random_movies[3]}',
-                        'movie5': f'{random_movies[4]}',
-                        'movie6': f'{random_movies[5]}',
-                        'movie7': f'{random_movies[6]}',
-                        'movie8': f'{random_movies[7]}',
-                        'movie9': f'{random_movies[8]}',
-                        'movie10': f'{random_movies[9]}'
-                        }
+                        random_movies
                     }
                 }
         final_json = json.dumps(final)
         return final_json
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)

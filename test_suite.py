e75218b5a6e93fe0c717e15f91180131a1b19ebe
William Meighan <williamdmeighan@gmail.com>
2020-06-03 16:54:04 -0600

    update test suite with api consumption tests

--- test_suite.py
+++ test_suite.py
@@ -3,6 +3,8 @@ import book_webscrape
 import movie_webscraper
 import events_api
 import wsgi
+import spotify_api
+import weather_api
 
 lat = '39.7392'
 long = '-104.9903'
@@ -23,6 +25,21 @@ five_events = events_api.getEvents(lat, long)
 
 def test_events():
     assert type(five_events) == dict
+
+
+spotify = spotify_api.get_spotify("sunny")
+
+def test_spotify_api():
+    assert type(spotify) == dict
+    assert spotify['playlists']['items'][0]['id']
+
+weather_data = weather_api.get_weather(lat, long)
+
+def test_weather_api():
+    assert type(weather_data) == dict
+    assert weather_data['current']
+    assert weather_data['hourly']
+    assert weather_data['daily']
 
 weather = wsgi.weather(lat, long)
 

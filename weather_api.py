49c4744cb1ece652ed0b8a38a115ec6001c49f43
William Meighan <williamdmeighan@gmail.com>
2020-06-03 16:53:44 -0600

    test weather api consumption

---
+++ weather_api.py
@@ -0,0 +1,11 @@
+import base64
+import os
+import requests
+import json
+
+def get_weather(lat, long):
+    weather_token = os.getenv('WEATHER_API_KEY')
+    weather_request = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+long+'&appid='+weather_token+'&units=imperial')
+    weather_json = weather_request.json()
+
+    return weather_json
